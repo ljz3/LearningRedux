2c183e97980fb463f90205ed434570fee6544022
ljz3 <kevin.ottawa@hotmail.com>
2021-01-04 22:00:28 -0500

    Able to play a default embedded video with video details and other related videos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDhBB3hOFooGRpir4uelZFrlIaUk7o2l5w';
 
-const App = () =>{
-  return <div>Hi</div>;
+
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {videos: [], selectedVideo: null};
+
+    YTSearch({key: API_KEY, term: 'etho'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    });
+
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar/>
+        <VideoDetail video = {this.state.selectedVideo}/>
+        <VideoList
+          videos = {this.state.videos}/>
+      </div>
+    );
+  }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
