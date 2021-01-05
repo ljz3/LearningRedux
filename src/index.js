d1aa07b35197e8ef38c5ff0461ae3503ae13a33c
ljz3 <kevin.ottawa@hotmail.com>
2021-01-04 23:04:13 -0500

    Now able to auto-search for videos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
@@ -15,19 +16,25 @@ class App extends Component{
 
     this.state = {videos: [], selectedVideo: null};
 
-    YTSearch({key: API_KEY, term: 'etho'}, (videos) => {
+    this.videoSearch('etho');
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       })
     });
-
   }
 
   render(){
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 500);
+
     return (
       <div>
-        <SearchBar/>
+        <SearchBar onSearchChange = {videoSearch}/>
         <VideoDetail video = {this.state.selectedVideo}/>
         <VideoList
           onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
